b4d279bedbf86874477a284a8f37b08fb489f92d
Drew Hollenback <drewsee26@gmail.com>
2017-03-10 20:52:23 +0000

    Assignment 32 second commit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+    def render_posts_for(user)
+        unless user.posts.any?
+            return "<h4>#{user.name} has not submitted any posts yet.</h4>".html_safe 
+        else
+            render user.posts
+        end
+    end
+    
+    def render_comments_for(user)
+        unless user.comments.any?
+            return "<h4>#{user.name} has not submitted any comments yet.</h4>".html_safe 
+        else
+            render user.comments
+        end
+    end
+    
+    def favorite_for(post)
+        render post.favorites
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,6 @@ class User < ActiveRecord::Base
         gravatar_id = Digest::MD5::hexdigest(self.email).downcase
         "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     end
+    
+    
 end
